b6de00e80ad910b6db218d39183e51eaaec6f670
Anthony Hansord <tonyhansorddev@gmail.com>
2024-03-30 21:04:15 +1100

    finished implementation of post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
@@ -18,12 +20,34 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def update
+    find_post
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
